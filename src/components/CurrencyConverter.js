44e8848b1b267d599ba603f3a184decc71015509
TalalMiftah <64993605+TalalMiftah@users.noreply.github.com>
2022-03-10 23:42:53 +0100

    Update CurrencyConverter.js

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -1,9 +1,7 @@
-import { useState } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from 'react'
 import { SwapOutlined } from '@ant-design/icons'
-import { Input,Card } from 'antd'
+import { Input, Card } from 'antd'
 import millify from 'millify'
-import { FontSizeOutlined } from '@ant-design/icons'
 const CurrencyConverter = () => {
 
     const currencies = ['BTC', 'ETH', 'USD', 'XRP', 'LTC', 'ADA', 'JPY', 'NEO']
@@ -17,7 +15,7 @@ const CurrencyConverter = () => {
     const [result, setResult] = useState(0)
 
 
-    const [exchangedData, setExchangedData] = useState( {
+    const [exchangedData, setExchangedData] = useState({
 
         primaryCurrency: 'BTC',
 
@@ -26,108 +24,78 @@ const CurrencyConverter = () => {
         exchangeRate: 0
 
     })
-
-
-
-    console.log(amount)
-
-    const convert = () => {
-
-        const options = {
-
-            method: 'GET',
-
-            url: 'https://alpha-vantage.p.rapidapi.com/query',
-
-            params: { to_currency: chosenSecondaryCurrency, function: 'CURRENCY_EXCHANGE_RATE', from_currency: chosenPrimaryCurrency },
-
-            headers: {
-
-                'x-rapidapi-host': 'alpha-vantage.p.rapidapi.com',
-                'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
-
-            }
-        }
-
-        axios.request(options).then((response) => {
-            setResult(response.data['Realtime Currency Exchange Rate']['5. Exchange Rate'] * amount)
-            setExchangedData({
-                primaryCurrency: chosenPrimaryCurrency,
-                secondaryCurrency: chosenSecondaryCurrency,
-                exchangeRate: response.data['Realtime Currency Exchange Rate']['5. Exchange Rate']
-            })
-        }).catch((error) => {
-            console.error(error)
+    async function fetchData() {
+        const response = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${chosenPrimaryCurrency}&tsyms=${chosenSecondaryCurrency}&api_key={4b73109b800662283c6a16d23d897c1520f7f6a91ccb60a65f8136790f34f946}`);
+        const json = await response.json();
+        setResult(json[chosenSecondaryCurrency] * amount);
+        setExchangedData({
+            primaryCurrency: chosenPrimaryCurrency,
+            secondaryCurrency: chosenSecondaryCurrency,
+            exchangeRate: json[chosenSecondaryCurrency],
         })
-    }
+    } useEffect(fetchData, []);
+    console.log(amount)
     return (
         <div className="currency-converter">
             <div className='input-box'>
-
-
-
                 <div className='cov'>
-                    
-                    <Card 
-                    hoverable
-                    style={
-                        {padding:"10px" , borderRadius: "10px"}
-                    }
+
+                    <Card
+                        hoverable
+                        style={
+                            { padding: "10px", borderRadius: "10px" }
+                        }
                     >
                         <h4>FROM</h4>
-                    <Input
-                        type="number"
-                        name="currency-amount-1"
-                        value={amount}
-                        onChange={(e) => setAmount(e.target.value)}
-                    />
-
-
-                    <select
-                        value={chosenPrimaryCurrency}
-                        name="currency-option-1"
-                        className="currency-options"
-                        onChange={(e) => setChosenPrimaryCurrency(e.target.value)}
-                    >
-                        {currencies.map((currency) => (<option >{currency}</option>))}
-                    </select>
+                        <Input
+                            type="number"
+                            name="currency-amount-1"
+                            value={amount}
+                            onChange={(e) => setAmount(e.target.value)}
+                        />
+                        <select
+                            value={chosenPrimaryCurrency}
+                            name="currency-option-1"
+                            className="currency-options"
+                            onChange={(e) => setChosenPrimaryCurrency(e.target.value)}
+                        >
+                            {currencies.map((currency) => (<option >{currency}</option>))}
+                        </select>
                     </Card>
                 </div>
-                <SwapOutlined className='covertb' id="convert-button" onClick={convert}></SwapOutlined>
+                <SwapOutlined className='covertb' id="convert-button" onClick={fetchData}></SwapOutlined>
                 <div className='cov'>
-                    
-                    <Card 
-                    hoverable
-                    style={
-                        { borderRadius: "10px",padding:'10px'}
-                    }
+
+                    <Card
+                        hoverable
+                        style={
+                            { borderRadius: "10px", padding: '10px' }
+                        }
                     >
                         <h4>TO</h4>
-                    <Input
-                        name="currency-amount-2"
-                        value={result}
-                    />
-
-
-                    <select
-                        value={chosenSecondaryCurrency}
-                        name="currency-option-2"
-                        className="currency-options"
-                        onChange={(e) => setChosenSecondaryCurrency(e.target.value)}
-                    >
-                        {currencies.map((currency) => (<option>{currency}</option>))}
-                    </select>
+                        <Input
+                            name="currency-amount-2"
+                            value={result}
+                        />
+                        <select
+                            value={chosenSecondaryCurrency}
+                            name="currency-option-2"
+                            className="currency-options"
+                            onChange={(e) => setChosenSecondaryCurrency(e.target.value)}
+                        >
+                            {currencies.map((currency) => (<option>{currency}</option>))}
+                        </select>
                     </Card>
                 </div>
-                
+
             </div>
             <div className="exchange-rate">
                 <Card>
-                <div className="excenter">
-                <h1>Exchange Rate</h1>
-                <h2>{millify(exchangedData.exchangeRate)}</h2>
-                <p  >{exchangedData.primaryCurrency} to {exchangedData.secondaryCurrency}</p>
-                </div>
+                    <div className="excenter">
+                        <h1>Exchange Rate</h1>
+                        <h2>{millify(exchangedData.exchangeRate)}</h2>
+                        <p  >{exchangedData.primaryCurrency} to {exchangedData.secondaryCurrency}</p>
+                    </div>
                 </Card>
             </div>
         </div>
